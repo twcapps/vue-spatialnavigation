6eff745da8c232f32c8dc7ef49a6f7f0965acbc3
Erik Lubbers <erik@twcapps.com>
2018-04-12 15:23:54 +0200

    Removed test/ from git ignore

---
+++ test/e2e/custom-assertions/elementEnter.js
@@ -0,0 +1,11 @@
+// A custom Nightwatch assertion.
+// The assertion name is the filename.
+// Example usage:
+//
+//   browser.assert.elementEnter(selector, expected)
+//
+// For more information on custom assertions see:
+// http://nightwatchjs.org/guide#writing-custom-assertions
+
+exports.assertion = function (selector, expected) {
+}
---
+++ test/e2e/custom-assertions/elementNavigate.js
@@ -0,0 +1,10 @@
+// A custom Nightwatch assertion.
+// The assertion name is the filename.
+// Example usage:
+//
+//
+// For more information on custom assertions see:
+// http://nightwatchjs.org/guide#writing-custom-assertions
+
+exports.assertion = function () {
+}
